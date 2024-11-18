c293bf00a5ea814014949dc6a9475dd2160b0f1a
mamillaharshit009 <mamillaharshit009@gmail.com>
2024-11-18 18:47:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def update_task(task_id):
     return render_template('assignment/update.html', task=task)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
